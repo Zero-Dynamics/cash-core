7b1378e14c943c117301f67539875eb0410656d8
hml-s <hml.sanders@outlook.com>
2024-03-20 06:40:26 +0100

    Update servicenode-sync

--- src/servicenode-sync.cpp
+++ src/servicenode-sync.cpp
@@ -359,7 +359,7 @@ void CServiceNodeSync::ProcessTick(CConnman& connman)
                 nRequestedServiceNodeAttempt++;
 
                 // ask node for all payment votes it has (new nodes will only return votes for future payments)
-                if (pnode->nVersion == 70900) {
+                if (pnode->nVersion == 70000) {
                     connman.PushMessage(pnode, msgMaker.Make(NetMsgType::SERVICENODEPAYMENTSYNC, snpayments.GetStorageLimit()));
                 } else {
                     connman.PushMessage(pnode, msgMaker.Make(NetMsgType::SERVICENODEPAYMENTSYNC));
