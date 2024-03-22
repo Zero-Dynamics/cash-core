ebc8c15264ac9a086daa7410f0cf4eee7ee3d709
hml-s <hml.sanders@outlook.com>
2024-03-22 23:04:28 +0100

    Update block.cpp

--- src/primitives/block.cpp
+++ src/primitives/block.cpp
@@ -11,11 +11,6 @@
 #include "tinyformat.h"
 #include "utilstrencodings.h"
 
-uint256 CBlockHeader::GetHash() const
-{
-    return hash_Argon2d(BEGIN(nVersion), END(nNonce), 1);
-}
-
 std::string CBlock::ToString() const
 {
     std::stringstream s;
