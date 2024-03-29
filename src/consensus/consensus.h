a133ddd039f5d46cdbf22f0374a118ff0e47ece4
Herbert Jr <23432027+hml-s@users.noreply.github.com>
2024-03-29 13:38:24 +0100

    Roconsider blocksizes

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -7,13 +7,13 @@
 #define CASH_CONSENSUS_CONSENSUS_H
 
 /** The maximum allowed size for a transaction, in bytes */
-static const unsigned int MAX_TX_SIZE = 1000000; // 1 MB to match the default maximum network send size.
+static const unsigned int MAX_TX_SIZE = 786432; // 768 KiB to match the default maximum network send size.
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
-static const unsigned int MAX_BLOCK_SIZE = 4194304; //4MB
+static const unsigned int MAX_BLOCK_SIZE = 1572864; // 1536KiB
 /** The maximum allowed number of signature check operations in a block (network rule) */
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE / 50;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
-static const int COINBASE_MATURITY = 10;
+static const int COINBASE_MATURITY = 40;
 
 /** Flags for nSequence and nLockTime locks */
 enum {
