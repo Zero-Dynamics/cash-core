99a06cae2d3a54a9764f9b6cbe6e668ff28ea4ba
Herbert Jr. <23432027+hml-s@users.noreply.github.com>
2024-08-16 02:02:12 +0200

    Add checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -169,10 +169,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_ISAUTOLOCKS].nThreshold = 3226; // 80% of 4032
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = 395000;
+        consensus.nMinimumChainWork = 400000;
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x000004785bb4e18e0b86c89aec51f81c8cbdb620e4046015b8b2768f460d658a"); //395000
+        consensus.defaultAssumeValid = uint256S("0x00000b0c6f3993b990dddbeddff974707c62b8484acb9bd29d23a2808c325fdd"); //400000
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -254,10 +254,8 @@ public:
                 (28800,     uint256S("0x00003d8a599764a660c8110041896499bd70d55579859089907ee08db77d7f84"))
                 (60000,     uint256S("0x000048983785dfe116c99a82312534d89da61e7fbe0bcc3794ee3754c552db82"))
                 (100000,    uint256S("0x00001b481bcdd0f8dd86739018e9c9bbd08405bf98a8c62e9c02c2d469f5c3c0"))
-                (200000,    uint256S("0x000005d799a0f5f31ff7d7968b7b6252dd603bedc03f8655f3e37e769635051d"))
-                (337376,    uint256S("0x000000d56a5af34dad69ba6258e9118a55211fc2af91d1ca83ba177f98a33956"))                
-                (395000,    uint256S("0x000004785bb4e18e0b86c89aec51f81c8cbdb620e4046015b8b2768f460d658a"))                
-                // (400000,    uint256S("0x"))
+                (200000,    uint256S("0x000005d799a0f5f31ff7d7968b7b6252dd603bedc03f8655f3e37e769635051d"))             
+                (400000,    uint256S("0x00000b0c6f3993b990dddbeddff974707c62b8484acb9bd29d23a2808c325fdd"))
                 // (1000000,   uint256S("0x"))
                 // (2000000,   uint256S("0x"))
                 // (3000000,   uint256S("0x"))
