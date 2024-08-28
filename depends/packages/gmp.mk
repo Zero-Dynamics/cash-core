933f2995203969d8344471a21ad5cf8a165af2dc
Herbert Jr. <23432027+hml-s@users.noreply.github.com>
2024-08-28 21:23:14 +0200

    [DEPENDS] Add gmp & backtrace

---
+++ depends/packages/gmp.mk
@@ -0,0 +1,27 @@
+package=gmp
+$(package)_version=6.3.0
+$(package)_download_path=https://gmplib.org/download/gmp
+$(package)_file_name=gmp-$($(package)_version).tar.bz2
+$(package)_sha256_hash=ac28211a7cfb609bae2e2c8d6058d66c8fe96434f740cf6fe2e47b000d1c20cb
+
+define $(package)_set_vars
+$(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-static
+$(package)_cflags_armv7l_linux+=-march=armv7-a
+$(package)_cflags_aarch64_darwin+=-march=armv8-a
+endef
+
+define $(package)_config_cmds
+  $($(package)_autoconf)
+endef
+
+define $(package)_build_cmds
+  $(MAKE)
+endef
+
+define $(package)_stage_cmds
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_postprocess_cmds
+  rm lib/*.la
+endef
