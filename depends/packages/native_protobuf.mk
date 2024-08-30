0765cb4c5c662b6b210fc04f874af386bfe6ec6d
Herbert Jr. <23432027+hml-s@users.noreply.github.com>
2024-08-30 23:12:40 +0200

    [DEPENDS] Update native

--- depends/packages/native_protobuf.mk
+++ depends/packages/native_protobuf.mk
@@ -1,11 +1,11 @@
 package=native_protobuf
-$(package)_version=3.6.0
+$(package)_version=3.17.3
 $(package)_download_path=https://github.com/google/protobuf/releases/download/v$($(package)_version)
-$(package)_file_name=protobuf-all-$($(package)_version).tar.gz
-$(package)_sha256_hash=1532154addf85080330fdd037949d4653dfce16550df5c70ea0cd212d8aff3af
+$(package)_file_name=protobuf-cpp-$($(package)_version).tar.gz
+$(package)_sha256_hash=51cec99f108b83422b7af1170afd7aeb2dd77d2bcbb7b6bad1f92509e9ccf8cb
 
 define $(package)_set_vars
-$(package)_config_opts=--disable-static
+$(package)_config_opts=--disable-static --without-zlib
 endef
 
 define $(package)_config_cmds
