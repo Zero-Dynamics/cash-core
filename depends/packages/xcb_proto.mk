fb892e5aa0b414819a822bc127301009be7b729b
Herbert Jr. <23432027+hml-s@users.noreply.github.com>
2024-09-04 00:27:46 +0200

    [DEPENDS] Revert xcbproto to 1.15

--- depends/packages/xcb_proto.mk
+++ depends/packages/xcb_proto.mk
@@ -1,13 +1,8 @@
 package=xcb_proto
-$(package)_version=1.17.0
-$(package)_download_path=http://xcb.freedesktop.org/dist
-$(package)_file_name=xcb-proto-$($(package)_version).tar.gz
-$(package)_sha256_hash=392d3c9690f8c8202a68fdb89c16fd55159ab8d65000a6da213f4a1576e97a16
-
-define $(package)_set_vars
-  $(package)_config_opts=--disable-static
-  $(package)_config_opts_linux=--with-pic
-endef
+$(package)_version=1.15.2
+$(package)_download_path=https://xorg.freedesktop.org/archive/individual/proto
+$(package)_file_name=xcb-proto-$($(package)_version).tar.xz
+$(package)_sha256_hash=7072beb1f680a2fe3f9e535b797c146d22528990c72f63ddb49d2f350a3653ed
 
 define $(package)_config_cmds
   $($(package)_autoconf)
@@ -22,6 +17,5 @@ define $(package)_stage_cmds
 endef
 
 define $(package)_postprocess_cmds
-  find -name "*.pyc" -delete && \
-  find -name "*.pyo" -delete
+  rm -rf lib/python*/site-packages/xcbgen/__pycache__
 endef
