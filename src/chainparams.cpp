2e54aa689d6afd4806d9ad3684e3e8ade76d7e63
Herbert Jr. <23432027+hml-s@users.noreply.github.com>
2024-09-04 14:49:49 +0200

    Update checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -169,10 +169,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_ISAUTOLOCKS].nThreshold = 3226; // 80% of 4032
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = 416000;
+        consensus.nMinimumChainWork = 436000;
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x000037c699c97b5a12b66c623eb7c4d7a6e9246b49a4cd559ed819680099eb57"); //416000
+        consensus.defaultAssumeValid = uint256S("0x000020d76b4dd001085398decc2f2a72ce3f79e8dcf54075e4f697c36f99e7fd"); //436000
 
         /**
          * The message start string is designed to be unlikely to occur in normal data.
@@ -256,7 +256,7 @@ public:
                 (100000,    uint256S("0x00001b481bcdd0f8dd86739018e9c9bbd08405bf98a8c62e9c02c2d469f5c3c0"))
                 (200000,    uint256S("0x000005d799a0f5f31ff7d7968b7b6252dd603bedc03f8655f3e37e769635051d"))             
                 (400000,    uint256S("0x00000b0c6f3993b990dddbeddff974707c62b8484acb9bd29d23a2808c325fdd"))
-                (416000,    uint256S("0x000037c699c97b5a12b66c623eb7c4d7a6e9246b49a4cd559ed819680099eb57"))
+                (436000,    uint256S("0x000020d76b4dd001085398decc2f2a72ce3f79e8dcf54075e4f697c36f99e7fd"))
                 // (2000000,   uint256S("0x"))
                 // (3000000,   uint256S("0x"))
         };
