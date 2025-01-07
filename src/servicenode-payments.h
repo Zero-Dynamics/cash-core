b127441362cf5c881e9c58afe652bb3ab276a9a6
Herbert Jr. <hml.sanders@outlook.com>
2025-01-07 19:36:34 +0100

    Bump protocol version

--- src/servicenode-payments.h
+++ src/servicenode-payments.h
@@ -25,7 +25,7 @@ static const int SNPAYMENTS_SIGNATURES_TOTAL = 20;
 // V1 - Last protocol version before update
 // V2 - Newest protocol version
 static const int MIN_SERVICENODE_PAYMENT_PROTO_VERSION_1 = 71110;
-static const int MIN_SERVICENODE_PAYMENT_PROTO_VERSION_2 = 71130; // Only ServiceNodes > v1.1.0.0 will get paid after Spork 10 activation
+static const int MIN_SERVICENODE_PAYMENT_PROTO_VERSION_2 = 71140; // Only ServiceNodes > v1.1.0.0 will get paid after Spork 10 activation
 
 extern CCriticalSection cs_vecPayees;
 extern CCriticalSection cs_mapServiceNodeBlocks;
--- src/version.h
+++ src/version.h
@@ -11,7 +11,7 @@
 /**
  * network protocol versioning
  */
-static const int PROTOCOL_VERSION = 71130;
+static const int PROTOCOL_VERSION = 71140;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
